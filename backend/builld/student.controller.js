c6c7a2308677a8ac2b0a4ac551337d48f3b0f8bd
Tyson <tysonpaul89@gmail.com>
2018-06-20 22:52:16 +0530

    ts removed

--- backend/builld/student.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = express_1.Router();
-router.get('/', function (req, res) {
-    res.send('Hello World');
-});
-exports.StudentController = router;
