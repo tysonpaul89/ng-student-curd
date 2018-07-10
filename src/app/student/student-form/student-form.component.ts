bb52f9b11ea532d1715baa9860f4ca9236a5f4ce
Tyson <tysonpaul89@gmail.com>
2018-07-10 23:11:10 +0530

    mergred local changes

--- src/app/student/student-form/student-form.component.ts
+++ src/app/student/student-form/student-form.component.ts
@@ -113,14 +113,17 @@ export class StudentFormComponent implements OnInit, OnChanges {
    * @param {string} date Date to format
    */
   private formatDate = (date) => {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+    const d = new Date(date);
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    const year = d.getFullYear();
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
+    if (month.length < 2) {
+      month = '0' + month;
+    }
+    if (day.length < 2) {
+      day = '0' + day;
+    }
     return [year, month, day].join('-');
   }
 
